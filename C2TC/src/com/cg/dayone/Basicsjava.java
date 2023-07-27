44a23cae0dc9f89eede9c9de47f32d4301b0e41b
Nidya03 <nidyathirshala@gmail.com>
2023-07-27 15:51:20 +0530

    dayone

--- C2TC/src/com/cg/dayone/Basicsjava.java
+++ C2TC/src/com/cg/dayone/Basicsjava.java
@@ -4,13 +4,14 @@ public class Basicsjava {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int a=76;
+		int a=20;
 		int b=3;
-		System.out.println(a+b);
-		System.out.println(a-b);
-		System.out.println(a++);
-		System.out.println(a%b);
-
+		System.out.println(a+b);//23
+		System.out.println(a-b);//17
+		System.out.println(a++);//20
+		System.out.println(a%b);//0
+		System.out.println(a/b);//7
+		System.out.println(--b);//2
 	}
 
 }
