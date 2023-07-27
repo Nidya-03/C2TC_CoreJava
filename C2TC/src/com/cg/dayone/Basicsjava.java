52ac96589c46de1547bb02b7f08ef23bcd071398
Nidya03 <nidyathirshala@gmail.com>
2023-07-27 15:10:56 +0530

    one day commit

--- C2TC/src/com/cg/dayone/Basicsjava.java
+++ C2TC/src/com/cg/dayone/Basicsjava.java
@@ -4,6 +4,14 @@ public class Basicsjava {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int a=20;
+		int b=3;
+		System.out.println(a+b);//23
+		System.out.println(a-b);//17
+		System.out.println(a++);//20
+		System.out.println(a%b);//0
+		System.out.println(a/b);//7
+		System.out.println(--b);//2
 
 	}
 
