ca862aad186644c68d4a2add49cd224991db1836
Nidya03 <nidyathirshala@gmail.com>
2023-07-28 15:24:39 +0530

    day one

---
+++ C2TC/src/com/cg/dayone/Identify.java
@@ -0,0 +1,12 @@
+//identifier
+package com.cg.dayone;
+
+public class Identify {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("full stack web development");
+
+	}
+
+}
---
+++ C2TC/src/com/cg/dayone/Primitivedt.java
@@ -0,0 +1,32 @@
+package com.cg.dayone;
+
+public class Primitivedt {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		boolean a = true;
+	    System.out.println(a); 
+	    byte b=69;
+	    System.out.println(b);
+	    short celcius=-144;
+	    System.out.println(celcius);
+	    int c=12;
+	    System.out.println(c);
+	    long d = 6233220L;
+	    System.out.println(d); 
+	    double e = -652.3;
+	    System.out.println(e);
+	    float f = -42.3f;
+	    System.out.println(f); 
+	    char letter = '\u0052';
+	    System.out.println(letter); 
+	    char l1 = '8';
+	    System.out.println(l1);  // prints 9
+	    	
+	    char l2 = 78;
+	    System.out.println(l2);
+	    
+
+	}
+
+}
