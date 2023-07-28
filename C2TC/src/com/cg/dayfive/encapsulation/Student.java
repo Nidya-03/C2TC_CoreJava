410ab86745f99d9bba250e273d907f02c1a615a3
Nidya03 <nidyathirshala@gmail.com>
2023-07-28 10:30:40 +0530

    dayfive

---
+++ C2TC/src/com/cg/dayfive/encapsulation/Student.java
@@ -0,0 +1,15 @@
+package com.cg.dayfive.encapsulation;
+
+public class Student {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Encapsule e=new Encapsule();
+		e.setName("nidya");
+		e.setAge(20);
+		e.setMail("nidya@gmail.com");
+		e.setPhno(63473724);
+		System.out.println(e.toString());
+	}
+
+}
